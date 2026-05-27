605ca3f122d0ca99dad9204380e37256d1063239
droberka <droberka@users.noreply.github.com>
2026-05-27 18:00:12 +0300

    Добавлен bot.py в корень

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
- 
+import sys
+import asyncio
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent))
+
+from bot.bot import bot, main
+
+if __name__ == "__main__":
+    asyncio.run(main())
